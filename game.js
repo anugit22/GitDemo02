d8e6c8b9df9bb2af073780caec24976aa9027241
anugit22 <anuradha.mondal2013@gmail.com>
2023-07-11 09:08:33 +0530

    hello

---
+++ game.js
@@ -0,0 +1,32 @@
+gamePattern = [];
+var buttonColours = ["red", "blue", "green", "yellow"];
+function nextSequence(){
+ var randomNumber = Math.floor(Math.random() * 3) + 1;
+
+var randomChosenColour = buttonColours[randomNumber];
+gamePattern.push(randomChosenColour);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var randomNumber = Math.random();
+// randomNumber = randomNumber * 6;
+// randomNumber = Math.floor(randomNumber);
+// }
